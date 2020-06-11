4859c0e43fd343b9c98dd0d20b36f402c1b655ea
Jason <jason.rostar@yahoo.com>
2020-06-11 16:03:45 -0700

    fixed formatting, removed props from products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { ProductContext } from '../contexts'
-
-// Components
 import Product from './Product';
 
-const Products = props => {
+
+const Products = () => {
 	const { products, addItem } = useContext(ProductContext)
+
 	return (
 		<div className="products-container">
 			{products.map(product => (
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,14 +1,9 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../contexts'
-
-
-// Components
 import Item from './ShoppingCartItem';
 
 const ShoppingCart = () => {
-
 	const { cart } = useContext(CartContext)
-
 	const getCartTotal = () => {
 		return cart.reduce((acc, value) => {
 			return acc + value.price;
